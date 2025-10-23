775fb8bb82fd3636e5c92c09c669cf59932d38e3
SThink <softthink02@gmail.com>
2025-10-23 19:13:37 +0200

    []

--- components/skill-path.tsx
+++ components/skill-path.tsx
@@ -15,25 +15,33 @@ type SkillPathProps = {
 
 export default function SkillPath({ levels, onSelectNode }: SkillPathProps) {
   return (
-    <div className="flex flex-col items-center gap-8">
-      {levels.map((row, rowIndex) => (
-        <div
-          key={rowIndex}
-          className="flex justify-center gap-6 relative"
-        >
-          {row.map((node) => (
-            <HexNode
-              key={node.id}
-              status={node.status}
-              icon={node.icon}
-              onClick={() => onSelectNode?.(node.id)}
-            />
-          ))}
-        </div>
-      ))}
+    <div className="flex flex-col items-center">
       <button className="mt-6 px-6 py-2 rounded-xl bg-yellow-500 text-black font-semibold hover:bg-yellow-400 transition-all shadow-md">
         Take Skill Quiz
       </button>
+
+      {levels.map((row, rowIndex) => (
+        <div
+          key={rowIndex}
+          className={`flex ${
+            rowIndex >= 1 && "justify-end w-[100%]"
+          } items-center relative mt-6`}
+        >
+          {row.map((node, ind) => (
+            <div key={node.id} className={`flex items-center`}>
+              <div className="w-12 h-[1px] bg-white my-auto" />
+              <HexNode
+                status={node.status}
+                icon={node.icon}
+                onClick={() => onSelectNode?.(node.id)}
+              />
+              {ind < row.length - 1 && (
+                <div className="w-12 h-[1px] bg-white my-auto" />
+              )}
+            </div>
+          ))}
+        </div>
+      ))}
     </div>
   );
 }
